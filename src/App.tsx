3259a309421e2cced7d222538f89f8e2bd74a90e
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-23 00:31:52 +0100

    feat: add the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-
-import "./App.css";
+import "./app.scss";
+import logo from "./assets/shared/logo.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <header className="header">
+        <a href="#" className="header__logo">
+          <img src={logo} alt="logo" />
+        </a>
+        <a href="#" className="header__preorderBtn">
+          Pre-order now
+        </a>
+      </header>
       <h1>Typemaster Keyboard</h1>
     </div>
   );
