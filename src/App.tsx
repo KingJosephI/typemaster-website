cc440076a244fc2e6eebd0d1cb05b2245352af75
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-23 01:46:02 +0100

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./app.scss";
 import logo from "./assets/shared/logo.svg";
+import heroImage from "./assets/mobile/image-keyboard.jpg";
 
 function App() {
   return (
@@ -8,11 +9,36 @@ function App() {
         <a href="#" className="header__logo">
           <img src={logo} alt="logo" />
         </a>
-        <a href="#" className="header__preorderBtn">
+        <a href="#" className="preorderBtn header__preorderBtn">
           Pre-order now
         </a>
       </header>
-      <h1>Typemaster Keyboard</h1>
+      <main>
+        <section className="hero">
+          <div className="hero__content">
+            <h1 className="hero__title">Typemaster Keyboard</h1>
+            <p className="hero__description">
+              Improve your productivity and gaming without breaking the bank.
+              Upgrade to a high quality mechanical typing experience.
+            </p>
+            <div className="hero__ctas">
+              <a href="#" className="preorderBtn hero__preorderBtn">
+                Pre-order now
+              </a>
+              <span className="hero__releaseDate">Release on 5/27</span>
+            </div>
+          </div>
+
+          <picture className="hero__picture">
+            <img src={heroImage} alt="keyboard" />
+          </picture>
+        </section>
+      </main>
+
+      <footer className="footer">
+        <span className="footer__title">Typemaster 2021 </span>| All Rights
+        Reserved
+      </footer>
     </div>
   );
 }
