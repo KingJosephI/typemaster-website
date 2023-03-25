06ab411e044cfcd6180109113cbe4d40d453ce21
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-25 18:48:29 +0100

    refactor(Hero): extract the component to style module

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,25 +1,29 @@
 import keyboardMobile from "../../assets/mobile/image-keyboard.jpg";
 import keyboardTablet from "../../assets/tablet/image-keyboard.jpg";
 import keyboardDesktop from "../../assets/desktop/image-keyboard.jpg";
+import heroStyles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <div className="hero__content">
-        <h1 className="hero__title">Typemaster Keyboard</h1>
-        <p className="hero__description">
+    <section className={heroStyles.hero}>
+      <div className={heroStyles.hero__content}>
+        <h1 className={heroStyles.hero__title}>Typemaster Keyboard</h1>
+        <p className={heroStyles.hero__description}>
           Improve your productivity and gaming without breaking the bank.
           Upgrade to a high quality mechanical typing experience.
         </p>
-        <div className="hero__ctas">
-          <a href="#" className="preorderBtn hero__preorderBtn">
+        <div className={heroStyles.hero__ctas}>
+          <a
+            href="#"
+            className={`${heroStyles.preorderBtn} ${heroStyles.hero__preorderBtn}`}
+          >
             Pre-order now
           </a>
-          <span className="hero__releaseDate">Release on 5/27</span>
+          <span className={heroStyles.hero__releaseDate}>Release on 5/27</span>
         </div>
       </div>
 
-      <picture className="hero__picture">
+      <picture className={heroStyles.hero__picture}>
         <source media="(min-width:1024px)" srcSet={keyboardDesktop} />
         <source media="(min-width:768px)" srcSet={keyboardTablet} />
         <img src={keyboardMobile} alt="keyboard" />
