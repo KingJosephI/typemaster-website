b590942df203fa14b4e148eef1c2b938cd5a1d77
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-25 17:54:21 +0100

    refactor: create an index file from which components are exported

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-// import heroImage from "./assets/mobile/image-keyboard.jpg";
-
-// import Header from "./components/Layout/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Features from "./components/Features/Features";
-import Keyboard from "./components/Keyboard/Keyboard";
-import Layout from "./components/Layout/Layout";
+import { Layout, Hero, Keyboard, Features } from "./components";
 
 function App() {
   return (
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Hero } from "./Hero/Hero";
+export { default as Keyboard } from "./Keyboard/Keyboard";
+export { default as Features } from "./Features/Features";
+export { default as Layout } from "./Layout/Layout";
