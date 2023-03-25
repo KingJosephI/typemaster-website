90ec2916db89e6a7e5b21d8c2066c78d5a6008c7
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-25 19:10:47 +0100

    refactor(Keyboard): extract style to its own module

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -4,27 +4,30 @@ import glassDesktop from "../../assets/desktop/image-glass-and-keyboard.jpg";
 import phoneMobile from "../../assets/mobile/image-phone-and-keyboard.jpg";
 import phoneDesktop from "../../assets/desktop/image-phone-and-keyboard.jpg";
 import phoneTablet from "../../assets/tablet/image-phone-and-keyboard.jpg";
+import keyboardStyles from "./Keyborad.module.scss";
 
 const Keyboard = () => {
   return (
-    <section className="keyboard">
-      <div className="keyboard__images">
+    <section className={keyboardStyles.keyboard}>
+      <div className={keyboardStyles.keyboard__images}>
         <picture>
           <source media="(min-width:1024px)" srcSet={phoneDesktop} />
           <source media="(min-width:768px)" srcSet={phoneTablet} />
-          <img src={phoneMobile} alt="" className="phone" />
-          <div className="overlay"></div>
+          <img src={phoneMobile} alt="" className={keyboardStyles.phone} />
+          <div className={keyboardStyles.overlay}></div>
         </picture>
 
         <picture>
           <source media="(min-width:1024px)" srcSet={glassDesktop} />
           <source media="(min-width:768px)" srcSet={glassTablet} />
-          <img src={glassMobile} alt="" className="glass" />
+          <img src={glassMobile} alt="" className={keyboardStyles.glass} />
         </picture>
       </div>
-      <div className="keyboard__content">
-        <h2 className="keyboard__title">Mechanical wireless Keyboard</h2>
-        <p className="keyboard__description">
+      <div className={keyboardStyles.keyboard__content}>
+        <h2 className={keyboardStyles.keyboard__title}>
+          Mechanical wireless Keyboard
+        </h2>
+        <p className={keyboardStyles.keyboard__description}>
           The Typemaster keyboard boasts top-notch build and practical design.
           It offers a wide variety of switches and keycaps, along with reliable
           wireless connectivity.
